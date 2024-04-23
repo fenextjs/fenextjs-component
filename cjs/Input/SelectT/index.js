43da1a8995e537742021e467bc4081fed1468f8b
Francisco Blanco <blancofrancisco34@gmail.com>
2024-04-23 10:44:40 -0500

    flb

--- cjs/Input/SelectT/index.js
+++ cjs/Input/SelectT/index.js
@@ -6,7 +6,7 @@ const react_1 = tslib_1.__importDefault(require("react"));
 const Select_1 = require("../Select");
 const InputSelectT = ({ defaultValue, value, options, onChange, onParse, ...props }) => {
     return (react_1.default.createElement(react_1.default.Fragment, null,
-        react_1.default.createElement(Select_1.InputSelect, { ...props, defaultValue: onParse(defaultValue), value: onParse(value), options: options.map(onParse), onChangeData: onChange })));
+        react_1.default.createElement(Select_1.InputSelect, { ...props, defaultValue: defaultValue ? onParse(defaultValue) : undefined, value: value ? onParse(value) : undefined, options: options.map(onParse), onChangeData: onChange })));
 };
 exports.InputSelectT = InputSelectT;
 //# sourceMappingURL=index.js.map
