b2fdfbbaeed02dfa1e1a3814b8d7d092276f9ac5
franciscoblancojn <blancofrancisco34@gmail.com>
2024-09-26 14:31:02 -0500

    update

--- cjs/AlertHook/index.d.ts
+++ cjs/AlertHook/index.d.ts
@@ -1,6 +1,21 @@
 import React from "react";
 import { _TProps } from "fenextjs-interface";
-import { useAlertProps } from "fenextjs-hook";
+import { AlertProps } from "fenextjs-interface";
+export interface useActionProps<T = any> {
+    name: string;
+    onActionExecute?: (d?: T) => void;
+}
+export declare const useAction: <T = any>({ name, onActionExecute, }: useActionProps<T>) => {
+    onAction: (detail?: T) => void;
+};
+export interface useAlertProps {
+    name?: string;
+}
+export declare const useAlert: <T = any>({ name, }: useAlertProps) => {
+    alert: AlertProps<T> | undefined;
+    setAlert: (detail?: AlertProps<T> | undefined) => void;
+    onClearAlert: () => void;
+};
 export interface AlertHookProps extends _TProps {
     className?: string;
     configHook?: useAlertProps;
