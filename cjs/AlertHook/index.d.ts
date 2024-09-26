2ddd3e5d751f2ecc396c949918101ad737f67332
franciscoblancojn <blancofrancisco34@gmail.com>
2024-09-26 14:32:24 -0500

    flb

--- cjs/AlertHook/index.d.ts
+++ cjs/AlertHook/index.d.ts
@@ -1,21 +1,6 @@
 import React from "react";
 import { _TProps } from "fenextjs-interface";
-import { AlertProps } from "fenextjs-interface";
-export interface useActionProps<T = any> {
-    name: string;
-    onActionExecute?: (d?: T) => void;
-}
-export declare const useAction: <T = any>({ name, onActionExecute, }: useActionProps<T>) => {
-    onAction: (detail?: T) => void;
-};
-export interface useAlertProps {
-    name?: string;
-}
-export declare const useAlert: <T = any>({ name, }: useAlertProps) => {
-    alert: AlertProps<T> | undefined;
-    setAlert: (detail?: AlertProps<T> | undefined) => void;
-    onClearAlert: () => void;
-};
+import { useAlertProps } from "fenextjs-hook";
 export interface AlertHookProps extends _TProps {
     className?: string;
     configHook?: useAlertProps;
--- cjs/AlertHook/index.js
+++ cjs/AlertHook/index.js
@@ -1,53 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AlertHook = exports.useAlert = exports.useAction = void 0;
+exports.AlertHook = void 0;
 const tslib_1 = require("tslib");
 const react_1 = tslib_1.__importDefault(require("react"));
+const fenextjs_hook_1 = require("fenextjs-hook");
 const Alert_1 = require("../Alert");
-const react_2 = require("react");
-const react_3 = require("react");
-const useAction = ({ name, onActionExecute, }) => {
-    const onLoad = () => {
-        if (!(window && typeof window != "undefined")) {
-            setTimeout(onLoad, 500);
-            return;
-        }
-        if (onActionExecute) {
-            window.addEventListener(`fenext-action-element-${name}`, (e) => {
-                const data = e?.detail;
-                onActionExecute?.(data);
-            }, false);
-        }
-    };
-    (0, react_3.useEffect)(onLoad, [onActionExecute]);
-    const onAction = (detail) => {
-        window.dispatchEvent(new CustomEvent(`fenext-action-element-${name}`, {
-            bubbles: true,
-            detail,
-        }));
-    };
-    return {
-        onAction,
-    };
-};
-exports.useAction = useAction;
-const useAlert = ({ name = "fenextjs-alert", }) => {
-    const [alert, setAlert] = (0, react_2.useState)(undefined);
-    const { onAction } = (0, exports.useAction)({
-        name,
-        onActionExecute: setAlert,
-    });
-    return {
-        alert,
-        setAlert: onAction,
-        onClearAlert: () => {
-            onAction(undefined);
-        },
-    };
-};
-exports.useAlert = useAlert;
 const AlertHook = ({ className = "", configHook = {}, ...props }) => {
-    const { alert, onClearAlert } = (0, exports.useAlert)(configHook);
+    const { alert, onClearAlert } = (0, fenextjs_hook_1.useAlert)(configHook);
     return (react_1.default.createElement(react_1.default.Fragment, null, alert && (react_1.default.createElement("div", { className: `fenext-alert-hook ${className}` },
         react_1.default.createElement(Alert_1.Alert, { ...props, ...alert, onClose: onClearAlert })))));
 };
