e02523785303782afbff39b4b5e8c438a757b5d4
franciscoblancojn <blancofrancisco34@gmail.com>
2024-12-28 22:21:34 -0500

    new reword colors

--- src/_.stories.tsx
+++ src/_.stories.tsx
@@ -18,99 +18,99 @@ const Profile: StoryFn<any> = () => (
         
         
         
-                />
+                >Test Children</Chat>
 
 <ChatMessage
                     
              id={"1"}
         
-                />
+                >Test Children</ChatMessage>
 
 <ChatFormSendMessage
                     
-                />
+                >Test Children</ChatFormSendMessage>
 
 <ChatUser
                     
-                />
+                >Test Children</ChatUser>
 
 <Text
                     
-                />
+                >Test Children</Text>
 
 <ContentShow
                     
-                />
+                >Test Children</ContentShow>
 
 <Title
                     
-                />
+                >Test Children</Title>
 
 <Breadcrumb
                     
              links={[]}
         
-                />
+                >Test Children</Breadcrumb>
 
 <SwichViewList
                     
-                />
+                >Test Children</SwichViewList>
 
 <SwichViewTable
                     
-                />
+                >Test Children</SwichViewTable>
 
 <SwichViewSelect
                     
-                />
+                >Test Children</SwichViewSelect>
 
 <TableActionCheckbox
                     
-                />
+                >Test Children</TableActionCheckbox>
 
 <MediaInput
                     
-                />
+                >Test Children</MediaInput>
 
 <MediaPage
                     
-                />
+                >Test Children</MediaPage>
 
 <Cols
                     
-                />
+                >Test Children</Cols>
 
 <Button
                     
-                />
+                >Test Children</Button>
 
 <LayoutGridMenuLeft
                     
-                />
+                >Test Children</LayoutGridMenuLeft>
 
 <LayoutGridMenuTopLeft
                     
-                />
+                >Test Children</LayoutGridMenuTopLeft>
 
 <LayoutGridMenuTop
                     
-                />
+                >Test Children</LayoutGridMenuTop>
 
 <Container
                     
-                />
+                >Test Children</Container>
 
 <StepsCircle
                     
-                />
+                >Test Children</StepsCircle>
 
 <ButtonMenu
                     
-                />
+                >Test Children</ButtonMenu>
 
 <Menu
                     
-                />
+                >Test Children</Menu>
 
 <ItemMenu
                     
@@ -118,119 +118,119 @@ const Profile: StoryFn<any> = () => (
             url="/"
 
         
-                />
+                >Test Children</ItemMenu>
 
 <Box
                     
-                />
+                >Test Children</Box>
 
 <InputNumber
                     
-                />
+                >Test Children</InputNumber>
 
 <InputText
                     
-                />
+                >Test Children</InputText>
 
 <InputCalendar
                     
-                />
+                >Test Children</InputCalendar>
 
 <InputSelectNumber
                     
-                />
+                >Test Children</InputSelectNumber>
 
 <InputGallery
                     
-                />
+                >Test Children</InputGallery>
 
 <InputUpload
                     
-                />
+                >Test Children</InputUpload>
 
 <InputScannerQr
                     
-                />
+                >Test Children</InputScannerQr>
 
 <InputScannerTextQr
                     
-                />
+                >Test Children</InputScannerTextQr>
 
 <InputCardNumber
                     
-                />
+                >Test Children</InputCardNumber>
 
 <InputCardExpDate
                     
-                />
+                >Test Children</InputCardExpDate>
 
 <InputCardCCV
                     
-                />
+                >Test Children</InputCardCCV>
 
 <InputSelectCity
                     
-                />
+                >Test Children</InputSelectCity>
 
 <InputSelect
                     
             options={[]}
         
-                />
+                >Test Children</InputSelect>
 
 <InputColor
                     
-                />
+                >Test Children</InputColor>
 
 <InputSelectTimeZone
                     
-                />
+                >Test Children</InputSelectTimeZone>
 
 <InputNumberCount
                     
-                />
+                >Test Children</InputNumberCount>
 
 <InputRate
                     
-                />
+                >Test Children</InputRate>
 
 <InputPhone
                     
-                />
+                >Test Children</InputPhone>
 
 <InputDate
                     
-                />
+                >Test Children</InputDate>
 
 <InputSelectCSC
                     
-                />
+                >Test Children</InputSelectCSC>
 
 <InputSelectCountryMultiple
                     
-                />
+                >Test Children</InputSelectCountryMultiple>
 
 <InputFileStatus
                     
             onUploadFile={async (e)=>e}
         
-                />
+                >Test Children</InputFileStatus>
 
 <InputRange
                     
-                />
+                >Test Children</InputRange>
 
 <InputGoogleLoadScript
                     
-                />
+                >Test Children</InputGoogleLoadScript>
 
 <InputGoogleMaps
                     
-                />
+                >Test Children</InputGoogleMaps>
 
 <InputGoogleAutocomplete
                     
-                />
+                >Test Children</InputGoogleAutocomplete>
 
 <InputSelectT
                     
@@ -243,38 +243,38 @@ const Profile: StoryFn<any> = () => (
                     }}
 
         
-                />
+                >Test Children</InputSelectT>
 
 <InputSelectMultiple
                     
                     options={[]}
 
         
-                />
+                >Test Children</InputSelectMultiple>
 
 <InputRadio
                     
-                />
+                >Test Children</InputRadio>
 
 <InputImg
                     
-                />
+                >Test Children</InputImg>
 
 <InputCode
                     
              maxLength={6}
         
-                />
+                >Test Children</InputCode>
 
 <InputTextSelect
                     
              options={[]}  
         
-                />
+                >Test Children</InputTextSelect>
 
 <InputPassword
                     
-                />
+                >Test Children</InputPassword>
 
 <InputSelectMultipleT
                     
@@ -286,107 +286,107 @@ const Profile: StoryFn<any> = () => (
                         }
                     }}
         
-                />
+                >Test Children</InputSelectMultipleT>
 
 <InputSelectCountry
                     
-                />
+                >Test Children</InputSelectCountry>
 
 <InputFile
                     
-                />
+                >Test Children</InputFile>
 
 <InputDateRange
                     
-                />
+                >Test Children</InputDateRange>
 
 <InputCheckbox
                     
-                />
+                >Test Children</InputCheckbox>
 
 <InputSwich
                     
-                />
+                >Test Children</InputSwich>
 
 <InputSearch
                     
-                />
+                >Test Children</InputSearch>
 
 <InputSelectState
                     
-                />
+                >Test Children</InputSelectState>
 
 <InputUnitVolumen
                     
-                />
+                >Test Children</InputUnitVolumen>
 
 <InputUnitBase
                     
              options={[]} 
         
-                />
+                >Test Children</InputUnitBase>
 
 <InputUnitDistance
                     
-                />
+                >Test Children</InputUnitDistance>
 
 <InputUnitWeight
                     
-                />
+                >Test Children</InputUnitWeight>
 
 <Counter
                     
             number={100}
         
-                />
+                >Test Children</Counter>
 
 <NotificationPop
                     
-                />
+                >Test Children</NotificationPop>
 
 <Notification
                     
-                />
+                >Test Children</Notification>
 
 <Copy
                     
-                />
+                >Test Children</Copy>
 
 <Slider
                     
-                />
+                >Test Children</Slider>
 
 <ImgGallery
                     
             imgs={[]} 
         
-                />
+                >Test Children</ImgGallery>
 
 <ImgSlider
                     
             imgs={[]}   
         
-                />
+                >Test Children</ImgSlider>
 
 <PaginationItemPage
                     
             nItems={10}
         
-                />
+                >Test Children</PaginationItemPage>
 
 <Pagination
                     
             nItems={10}  
         
-                />
+                >Test Children</Pagination>
 
 <PaginationNPage
                     
-                />
+                >Test Children</PaginationNPage>
 
 <AlertHook
                     
-                />
+                >Test Children</AlertHook>
 
 <Table
                     
@@ -394,185 +394,185 @@ const Profile: StoryFn<any> = () => (
             items={[]}
             name="table"
         
-                />
+                >Test Children</Table>
 
 <Alert
                     
                  message="alert" 
                  type="WARNING"  
         
-                />
+                >Test Children</Alert>
 
 <Collapse
                     
              header={<></>}
         
-                />
+                >Test Children</Collapse>
 
 <CollapseMultiple
                     
-                />
+                >Test Children</CollapseMultiple>
 
 <LavaLamp
                     
-                />
+                >Test Children</LavaLamp>
 
 <ErrorComponent
                     
-                />
+                >Test Children</ErrorComponent>
 
 <Form
                     
-                />
+                >Test Children</Form>
 
 <Tooltip
                     
-                />
+                >Test Children</Tooltip>
 
 <TwoColSticky
                     
-                />
+                >Test Children</TwoColSticky>
 
 <Tab
                     
-                />
+                >Test Children</Tab>
 
 <DropDown
                     
              header={<></>}
         
-                />
+                >Test Children</DropDown>
 
 <PrintIframe
                     
             onComponent={()=>""}  
             url=""
         
-                />
+                >Test Children</PrintIframe>
 
 <PrintPage
                     
             onComponent={()=>""}  
         
-                />
+                >Test Children</PrintPage>
 
 <Steps
                     
             items={[]}
         
-                />
+                >Test Children</Steps>
 
 <Back
                     
-                />
+                >Test Children</Back>
 
 <Theme
                     
-                />
+                >Test Children</Theme>
 
 <Link
                     
             href={"/"}        
         
-                />
+                >Test Children</Link>
 
 <Share
                     
-                />
+                >Test Children</Share>
 
 <ContentLoading
                     
-                />
+                >Test Children</ContentLoading>
 
 <GridGallery
                     
             items={[]}
         
-                />
+                >Test Children</GridGallery>
 
 <ChronologicalList
                     
              items={[]}
         
-                />
+                >Test Children</ChronologicalList>
 
 <User
                     
-                />
+                >Test Children</User>
 
 <Img
                     
             src=""
         
-                />
+                >Test Children</Img>
 
 <DesignTypography
                     
-                />
+                >Test Children</DesignTypography>
 
 <DesignBox
                     
-                />
+                >Test Children</DesignBox>
 
 <Portal
                     
             children=""
         
-                />
+                >Test Children</Portal>
 
 <PageProgress
                     
-                />
+                >Test Children</PageProgress>
 
 <FilterDate
                     
-                />
+                >Test Children</FilterDate>
 
 <ScheduleDay
                     
-                />
+                >Test Children</ScheduleDay>
 
 <ScheduleWeekly
                     
-                />
+                >Test Children</ScheduleWeekly>
 
 <Template
                     
-                />
+                >Test Children</Template>
 
 <Modal
                     
-                />
+                >Test Children</Modal>
 
 <ProgressCircle
                     
             p={10}
             showP={true}
         
-                />
+                >Test Children</ProgressCircle>
 
 <ProgressLine
                     
             p={10}
             showP={true}
         
-                />
+                >Test Children</ProgressLine>
 
 <Chronometer
                     
-                />
+                >Test Children</Chronometer>
 
 <LoaderLine
                     
-                />
+                >Test Children</LoaderLine>
 
 <LoaderUser
                     
-                />
+                >Test Children</LoaderUser>
 
 <LoaderSpinner
                     
-                />
+                >Test Children</LoaderSpinner>
     </div>
 );
 export const Index = Profile.bind({});
