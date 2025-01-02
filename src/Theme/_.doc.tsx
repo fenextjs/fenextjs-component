a9426876d7f8ac4447a611316230b5a594190918
franciscoblancojn <blancofrancisco34@gmail.com>
2025-01-02 11:07:30 -0500

    fix colors scrolls and add fenext-prefers-color-scheme-disabled

--- src/Theme/_.doc.tsx
+++ src/Theme/_.doc.tsx
@@ -31,4 +31,24 @@ export default {
             content: `<Theme className="custom-theme" classNameItem="custom-theme-item" />`,
         },
     ],
+    extras: [
+        {
+            id: "NOTA",
+            title: "__NOTA__",
+            description:
+                "Fenextjs usa por defecto el `Tema Automático del Navegador`. En caso de que quieras deshabilitar esta funcionalidad sin usar el componente `Theme`, puedes agregar esta propiedad: `fenext-prefers-color-scheme-disabled` en cualquier etiqueta de HTML.",
+            code: `<body fenext-prefers-color-scheme-disabled>
+    contenido
+</body>
+
+//O
+
+<body>
+    contenido
+    <div fenext-prefers-color-scheme-disabled/>
+</body>
+`,
+        },
+    ]
+    
 };
