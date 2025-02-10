14ae19bef3057d4a587d1733b1a2cb07d68fc682
franciscoblancojn <blancofrancisco34@gmail.com>
2025-02-10 08:47:40 -0500

    doc

--- src/GridCols/_.doc.tsx
+++ src/GridCols/_.doc.tsx
@@ -1,55 +1,93 @@
 export default {
-    id: "cols",
-    idStorybook: "component-cols",
-    name: "Cols",
+    id: "grid-cols",
+    idStorybook: "component-grid-cols",
+    name: "GridCols",
     description:
-        "El componente Cols permite organizar elementos en múltiples columnas, con opciones para controlar la cantidad de columnas y la propiedad de ruptura interna de los elementos.",
+        "El componente GridCols permite organizar elementos en una cuadrícula flexible, con la posibilidad de definir diferentes configuraciones de columnas según el tamaño de la pantalla.",
     props: [
-        {
-            id: "nCols",
-            type: "number | string",
-            require: false,
-            default: "2",
-            description:
-                "Número de columnas en las que se organizarán los elementos. Puede ser un valor numérico o una cadena.",
-        },
-        {
-            id: "breakInside",
-            type: "boolean",
-            require: false,
-            default: "true",
-            description:
-                "Indica si se debe aplicar la propiedad 'break-inside' para evitar la ruptura interna de los elementos dentro de las columnas.",
-        },
         {
             id: "children",
             type: "ReactNode",
             require: false,
             default: "undefined",
-            description:
-                "Contenido o elementos que se mostrarán dentro de las columnas.",
+            description: "Contenido o elementos que se mostrarán dentro de la cuadrícula.",
         },
         {
             id: "className",
             type: "string",
             require: false,
             default: "''",
-            description:
-                "Clase CSS para personalizar el contenedor de las columnas.",
+            description: "Clase CSS adicional para personalizar el contenedor de la cuadrícula.",
         },
+        {
+            id: "cols",
+            type: "string",
+            require: false,
+            default: "'1fr'",
+            description: "Configuración de columnas por defecto usando CSS Grid.",
+        },
+        {
+            id: "colsMin1920",
+            type: "string",
+            require: false,
+            default: "undefined",
+            description: "Configuración de columnas para pantallas con un ancho mínimo de 1920px.",
+        },
+        {
+            id: "colsMin1680",
+            type: "string",
+            require: false,
+            default: "undefined",
+            description: "Configuración de columnas para pantallas con un ancho mínimo de 1680px.",
+        },
+        {
+            id: "colsMin1440",
+            type: "string",
+            require: false,
+            default: "undefined",
+            description: "Configuración de columnas para pantallas con un ancho mínimo de 1440px.",
+        },
+        {
+            id: "colsMin1024",
+            type: "string",
+            require: false,
+            default: "undefined",
+            description: "Configuración de columnas para pantallas con un ancho mínimo de 1024px.",
+        },
+        {
+            id: "colsMin992",
+            type: "string",
+            require: false,
+            default: "undefined",
+            description: "Configuración de columnas para pantallas con un ancho mínimo de 992px.",
+        },
+        {
+            id: "colsMin768",
+            type: "string",
+            require: false,
+            default: "undefined",
+            description: "Configuración de columnas para pantallas con un ancho mínimo de 768px.",
+        },
+        {
+            id: "colsMin575",
+            type: "string",
+            require: false,
+            default: "undefined",
+            description: "Configuración de columnas para pantallas con un ancho mínimo de 575px.",
+        }
     ],
     useExample: [
         {
-            text: "Básico",
-            content: `<Cols><div>Item 1</div><div>Item 2</div></Cols>`,
+            text: "Uso básico",
+            content: `<GridCols><div>Item 1</div><div>Item 2</div></GridCols>`,
         },
         {
-            text: "Cols con 3 columnas",
-            content: `<Cols nCols={3}><div>Item 1</div><div>Item 2</div><div>Item 3</div></Cols>`,
+            text: "GridCols con diferentes columnas",
+            content: `<GridCols cols="repeat(3, 1fr)"><div>Item 1</div><div>Item 2</div><div>Item 3</div></GridCols>`,
         },
         {
-            text: "Cols con breakInside deshabilitado",
-            content: `<Cols breakInside={false}><div>Item 1</div><div>Item 2</div></Cols>`,
+            text: "GridCols con configuración responsiva",
+            content: `<GridCols colsMin768="repeat(2, 1fr)" colsMin1024="repeat(3, 1fr)"><div>Item 1</div><div>Item 2</div><div>Item 3</div></GridCols>`,
         },
     ],
 };
